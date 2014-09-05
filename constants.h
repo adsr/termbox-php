4474fddbf51c3e6252a15f7e7325a04ce3093a26
Adam Saponara <saponara@gmail.com>
2014-09-05 19:08:16 -0400

    change to Apache 2 license

--- constants.h
+++ constants.h
@@ -1,3 +1,25 @@
+/*
+  +----------------------------------------------------------------------+
+  | termbox                                                              |
+  +----------------------------------------------------------------------+
+  | Licensed under the Apache License, Version 2.0 (the "License"); you  |
+  | may not use this file except in compliance with the License. You may |
+  | obtain a copy of the License at                                      |
+  | http://www.apache.org/licenses/LICENSE-2.0                           |
+  |                                                                      |
+  | Unless required by applicable law or agreed to in writing, software  |
+  | distributed under the License is distributed on an "AS IS" BASIS,    |
+  | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      |
+  | implied. See the License for the specific language governing         |
+  | permissions and limitations under the License.                       |
+  +----------------------------------------------------------------------+
+  | Author: Adam Saponara <adam@atoi.cc>                                 |
+  +----------------------------------------------------------------------+
+*/
+
+#ifndef PHP_TERMBOX_CONSTANTS_H
+#define PHP_TERMBOX_CONSTANTS_H
+
 PHP_TERMBOX_CONSTANT(TB_KEY_F1)
 PHP_TERMBOX_CONSTANT(TB_KEY_F2)
 PHP_TERMBOX_CONSTANT(TB_KEY_F3)
@@ -93,3 +115,5 @@ PHP_TERMBOX_CONSTANT(TB_OUTPUT_256)
 PHP_TERMBOX_CONSTANT(TB_OUTPUT_216)
 PHP_TERMBOX_CONSTANT(TB_OUTPUT_GRAYSCALE)
 PHP_TERMBOX_CONSTANT(TB_EOF)
+
+#endif
